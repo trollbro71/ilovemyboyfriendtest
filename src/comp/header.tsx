2acdeca7bac28e62761fa0dca1eba70d58702e36
Thomas Tran <thomaht3@uci.edu>
2025-05-28 18:20:01 -0700

    i dont wanna talk about it

--- src/comp/header.tsx
+++ src/comp/header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './header.css';
+import headerPhoto from '../assets/header.jpg';
 
 const Header: React.FC = () => {
   return (
     <header className="gray-header">
       <div className="header-content">
-        <img src="header.jpg" alt="Profile" className="header-photo" />
+        <img src={headerPhoto} alt="Profile" className="header-photo" />
         <div className="header-text">pissbowl</div>
       </div>
     </header>
