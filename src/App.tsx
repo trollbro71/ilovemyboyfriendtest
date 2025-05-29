3ba790c92e437face832f4c5cd603f0199a4b722
Thomas Tran <thomaht3@uci.edu>
2025-05-28 17:56:13 -0700

    last min bug just killl me

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GLBViewer url="/model.glb" />
+        <GLBViewer url={`${process.env.PUBLIC_URL}/model.glb`} />
       </header>
     </div>
   );
